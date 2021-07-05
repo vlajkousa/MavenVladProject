227b1b26bed7c92661d638700dc28e2f82238110
Vlajkousa <vargirovic@yahoo.com>
2021-07-04 21:25:06 -0400

    added mor work

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,6 +2,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing more features");
+        System.out.println("Git Hub is already published ");
+        System.out.println("Doing more work");
     }
 }
 
